e23cc00bb8884b084a24782eb2cc722bdd6076c2
Bob Stoyneff <bstoyneff@yprime.com>
2019-07-19 13:32:56 +0300

    update transaction listview

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from transaction.models import Transaction
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('paypal', 'member', 'amount', 'description', 'date')
+
 # Register your models here.
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
